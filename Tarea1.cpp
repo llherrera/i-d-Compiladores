801b4c2d9ee5c70dd498bf8af7d53d55ae94e278
Leonardo <leonardoh050@gmail.com>
2022-08-21 20:21:22 -0500

    otro commit

---
+++ Tarea1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string cadena, res;
+    cout << "Ingrese la cadena" << endl;
+    getline(cin, cadena);
+    regex expReg("\\s+");
+    res = regex_replace(cadena, expReg, "-");
+    cout << res << endl;
+    return 0;
+}
