601fa2ac4af31182864a949c53204b5939928db8
Leonardo <leonardoh050@gmail.com>
2022-08-25 20:46:40 -0500

    avance de la tarea 2

--- Tarea2.cpp
+++ Tarea2.cpp
@@ -9,8 +9,8 @@ int main(int argc, char const *argv[])
     string cadena, res;
     cout << "Ingrese la cadena" << endl;
     getline(cin, cadena);
-    regex expReg("\\s+");
-    res = regex_replace(cadena, expReg, "-");
+    regex expReg("^\\s |\\s$ |\\s\\s+");
+    res = regex_replace(cadena, expReg, "");
     cout << res << endl;
     return 0;
 }
